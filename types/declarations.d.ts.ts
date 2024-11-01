977e13720fd30047539d843fae5bde0a4dd8a294
sanijae <muhammadsanijae@gmail.com>
2024-11-01 16:30:09 +0100

    Slider solve

--- types/declarations.d.ts.ts
+++ types/declarations.d.ts.ts
@@ -1,8 +1,8 @@
-// declare module "react-slick" {
-//   import { ComponentType } from "react";
-//   const Slider: ComponentType<any>;
-//   export = Slider;
-// }
+declare module "react-slick" {
+  import { ComponentType } from "react";
+  const Slider: ComponentType<any>;
+  // export = Slider;
+}
 
 
-// export {}; 
+export {}; 
